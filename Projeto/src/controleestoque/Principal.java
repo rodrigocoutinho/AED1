1afb20432d94644537aa0b677641c8ab1d5ce9f8
Lucas Mendes <57737521+lucasmendeslms@users.noreply.github.com>
2021-04-16 21:45:57 -0300

    Implementação do Menu

--- Projeto/src/controleestoque/Principal.java
+++ Projeto/src/controleestoque/Principal.java
@@ -6,6 +6,10 @@ public class Principal {
     public static void main(String[] args){
        
         JOptionPane.showMessageDialog(null, "Olá, seja bem vindo ao seu sistema de controle de estoque.");
-        
+
+        Menu menu = new Menu();
+
+        menu.iniciarMenu();
+   
     }
 }
--- Projeto/src/controleestoque/Produto.java
+++ Projeto/src/controleestoque/Produto.java
@@ -1,6 +1,6 @@
 package controleestoque;
 
-//import javax.swing.JOPtionPane;
+import javax.swing.JOptionPane;
 
 public class Produto {
 
@@ -9,12 +9,43 @@ public class Produto {
     private int quantidade;
     private int id;
 
-    public Produto(String nome, double preco){
+    public Produto(int id, String nome, int quantidade, double preco){
+        this.id = id;
         this.nome = nome;
+        this.quantidade = quantidade;
         this.preco = preco;
     }
 
+    public Produto(){}
 
+    public int getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public double getQuantidade(){
+        return quantidade;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+
+    public void cadastrarProduto(){
+
+        JOptionPane.showMessageDialog(null,"Testando");
+    }
 
 
 }
