8216e6b2e485e8311603597fa8f70efa16ff6458
Lucas Mendes <57737521+lucasmendeslms@users.noreply.github.com>
2021-04-22 21:54:29 -0300

    Implementação da função cadastrarProduto()

--- Estoque/Sistema/Menu.java
+++ Estoque/Sistema/Menu.java
@@ -1,39 +1,38 @@
+
 //public class Menu {
 import javax.swing.JOptionPane;
 
 public class Menu {
 
-    protected Menu(){}
+    protected Menu() {
+    }
 
-    protected void iniciarMenu(){
+    protected void iniciarMenu() {
 
         int resposta;
         Produto produto = new Produto();
-        
 
         JOptionPane.showMessageDialog(null,
-        "O que você deseja fazer?\n\n\n1 - Cadastrar produto\n\n2 - Excluir produto\n\n3 - Adicionar produto ao estoque\n\n4- Remover produto do estoque\n\n5 - Exibir todos os produtos em estoque\n\n6 - Exibir solicitações pendentes\n\n7 - Atender solicitação de produto\n\n8 - Cadastrar fornecedor\n\n9 - Alterar dados de um fornecedor\n\n10 - Excluir fornecedor\n\n11 - Solicitar produto\n\n0 - Sair" );
+                "O que você deseja fazer?\n\n\n1 - Cadastrar produto\n\n2 - Excluir produto\n\n3 - Adicionar produto ao estoque\n\n4- Remover produto do estoque\n\n5 - Exibir todos os produtos em estoque\n\n6 - Exibir solicitações pendentes\n\n7 - Atender solicitação de produto\n\n8 - Cadastrar fornecedor\n\n9 - Alterar dados de um fornecedor\n\n10 - Excluir fornecedor\n\n11 - Solicitar produto\n\n0 - Sair");
 
         resposta = Integer.parseInt(JOptionPane.showInputDialog(""));
 
-        switch(resposta){
+        switch (resposta) {
 
-            case 0:
-                JOptionPane.showMessageDialog(null, "Tudo bem, até breve!!");
-                break;
+        case 0:
+            JOptionPane.showMessageDialog(null, "Tudo bem, até breve!!");
+            break;
 
-            case 1:
-                produto.cadastrarProduto();
-                break;
+        case 1:
+            produto.cadastrarProduto();
+            break;
 
-            default:
-                JOptionPane.showMessageDialog(null, "Valor invalido");
-                iniciarMenu();
+        default:
+            JOptionPane.showMessageDialog(null, "Valor invalido");
+            iniciarMenu();
 
         }
 
     }
 
-    
 }
-
--- Estoque/Sistema/Produto.java
+++ Estoque/Sistema/Produto.java
@@ -1,44 +1,71 @@
-//public class Produto {
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class Produto {
 
     private String nome;
-    private double preco;
-    private int quantidade;
-    private int id;
+    private Double preco;
+    private Integer quantidade;
+    private Integer id;
 
-    protected Produto(){}
-
-    public int getId(){
-        return id;
+    protected Produto() {
     }
 
-    public String getNome(){
-        return nome;
-    }
-
-    public void setNome(String nome){
+    protected Produto(Integer id, String nome, Integer quantidade, Double preco) {
+        this.id = id;
         this.nome = nome;
-    }
-
-    public double getQuantidade(){
-        return quantidade;
-    }
-
-    public double getPreco(){
-        return preco;
-    }
-
-    public void setPreco(double preco){
+        this.quantidade = quantidade;
         this.preco = preco;
     }
 
-    public void cadastrarProduto(){
+    int resposta = 0;
+    List<Produto> listaProdutos = new ArrayList<>();
+    Menu menu = new Menu();
 
-        JOptionPane.showMessageDialog(null,"Testando");
+    public int getId() {
+        return id;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void cadastrarProduto() {
+
+        do {
+            int ident = Integer.parseInt(JOptionPane.showInputDialog("Informe o ID do produto que deseja cadastrar:"));
+
+            String name = JOptionPane.showInputDialog("Informe o nome do produto:");
+
+            int quantity = Integer.parseInt(JOptionPane
+                    .showInputDialog("Informe a quantidade inicial de produtos que serão adicionados ao estoque"));
+            double price = Double
+                    .parseDouble(JOptionPane.showInputDialog("Informe o preço inicial do produto que será adicionado"));
+            listaProdutos.add(new Produto(ident, name, quantity, price));
+
+            resposta = JOptionPane.showConfirmDialog(null, "Deseja cadastrar um novo produto?");
+
+        } while (resposta == JOptionPane.YES_OPTION);
+
+        menu.iniciarMenu();
+
+    }
 
 }
-
